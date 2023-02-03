2b37c7ec1b915d0e99bf2b894b09e8494669030d
Garikoitz Lerma-Usabiaga <garikoitz@gmail.com>
2023-02-03 11:36:53 +0100

    sending code to david after meeting feb 3

--- main_script.m
+++ main_script.m
@@ -1,3 +1,6 @@
+
+% TODO meeting 9th: (1) paste figures and discuss layout, (2) discuss how to visualize and compare time series to explain the results
+
 %%
 clear all; close all;
 
@@ -158,3 +161,90 @@ for nr=1:3
     saveas(gcf, fullfile(cr.dirs.FIGSVG,[titlefile '.svg']), 'svg')
 end
 close all
+
+
+%% After seing the individuals, remove 25 and 26 and create group analyses
+%% Launch the function
+% fname = 'CoverageBoot_varexp02_';%'Fig1_'; % '' for not saving
+list_subInds_no2526      = [1:24,27:28];
+
+for nr=1:3
+    ALL2d=RF_individuals{nr,1};
+    ALL3d=RF_individuals{nr,2};
+    
+    ALL2d = ALL2d(:,:,list_subInds_no2526);
+    ALL3d = ALL3d(:,:,list_subInds_no2526);
+
+
+    MVALS = zeros(128,128,50);
+    SVALS = zeros(128,128,50);
+    DVALS = zeros(128,128,50);
+    
+    for kk=1:50
+        % Remove 1 each time and create same plots with the remaining one
+        nsubsminus1 = size(ALL2d,3)-1;
+        randReplacement = datasample(1:nsubsminus1,nsubsminus1);
+        all2d           = ALL2d(:,:,randReplacement);
+        all3d           = ALL3d(:,:,randReplacement);
+        
+        % Calculate measures
+        mval   = mean(all2d - all3d, 3);
+        stdval = std(all2d - all3d, [],3);
+        Cd     = zeros(128,128);
+        for ii=1:128;for jj=1:128
+                Cd(ii,jj)=computeCohen_d(all2d(ii,jj,:),all3d(ii,jj,:),'paired');
+        end;end
+    
+        % Accummlate it
+        MVALS(:,:,kk) = mval;
+        SVALS(:,:,kk) = stdval;
+        DVALS(:,:,kk) = Cd;
+        
+    end   
+    
+    % obtain means again
+    mval   = mean(MVALS,3);
+    stdval = mean(SVALS,3);
+    Cd     = mean(DVALS,3);
+    
+    
+    % PLOT
+    xdeg = 8; ydeg = 8;
+    mrvNewGraphWin('bootstraps2d3dFOV','wide',true);
+    subplot(1,2,1)
+    imagesc(Cd);axis equal;colormap(parula);colorbar;grid
+    title("d': mean 50 bootstraps [2D-3D]")
+    xlim([1,128]);ylim([1,128])
+    xticks([1,64,128]); yticks([1,64,128])
+    % caxis([-1.25,1.25])
+    caxis([-1,2])
+    xticklabels([-xdeg,0,xdeg]); yticklabels([-ydeg,0,ydeg])
+    xlabel('Degs'); ylabel('Degs')
+    
+    % subplot(1,2,2)
+    subplot(1,2,2)
+    [X,Y] = meshgrid(1:128,1:128);
+    XX = ((X-64)/64)*xdeg;
+    YY = ((Y-64)/64)*ydeg;
+    YY = flipud(YY);
+    surf(XX,YY,Cd);
+    xlabel('X (degs)'); ylabel('Y (degs)')
+    zlabel("Cohen's d")
+    xlim([-xdeg,xdeg])
+    ylim([-ydeg,ydeg])
+    zlim([-1,2])
+    xticks([-8,-4,0,4,8])
+    yticks([-8,-4,0,4,8])
+    xticklabels({'-8','-4','0','4','8'})
+    yticklabels({''})
+    set(gca,'FontSize',18)
+    
+    fname = ['FOV_Comparisons_ROI-' list_roiNames{nr} '_2dvs3d_vfc.method_avg_varexp02_NO-25-26'];
+    set(0, 'DefaultFigureRenderer', 'painters');
+    saveas(gcf, fullfile(cr.dirs.FIGPNG, [fname '.png']), 'png')
+    saveas(gcf, fullfile(cr.dirs.FIGSVG,[fname '.svg']), 'svg')
+end
+
+
+
+
